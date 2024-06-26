1323b0f36f9f80658504ead08265da091140d18b
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-26 21:07:10 +0530

    Redis and Bullmq init

---
+++ src/config/redis.config.ts
@@ -0,0 +1,12 @@
+import { Redis } from 'ioredis';
+
+import serverConfig from './server.config';
+
+const redisConfig = {
+    port: serverConfig.REDIS_PORT,
+    host: serverConfig.REDIS_HOST
+};
+
+const redisConnection = new Redis(redisConfig);
+
+export default redisConnection;
