4932f9d97d3097320bbdf03a449a742773055492
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-19 22:20:55 +0530

    refactor removed unnessary code

--- src/validators/zod.validator.ts
+++ src/validators/zod.validator.ts
@@ -3,7 +3,6 @@ import { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 
 import { CreateSubmissionDto } from "../dtos/createSubmission.dto";
-import { AddEvaluationDto } from "../dtos/addEvaluation.dto";
 import logger from "../config/logger.config";
 
 export const validateCreateSubmissionDto = (schema: ZodSchema<CreateSubmissionDto>) => (req: Request, res: Response, next: NextFunction) => {
@@ -18,23 +17,6 @@ export const validateCreateSubmissionDto = (schema: ZodSchema<CreateSubmissionDt
             success: false,
             error: error,
             message: 'Something went Wrong in Validating submission',
-            data: {}
-        });
-    }
-};
-
-export const validateAddEvaluationDto = (schema: ZodSchema<AddEvaluationDto>) => (req: Request, res: Response, next: NextFunction) => {
-    try {
-        schema.parse({
-            ...req.body
-        });
-        next();
-    } catch (error) {
-        logger.error(`Error in validating Evaluation: ${error}`);
-        res.status(StatusCodes.BAD_REQUEST).json({
-            success: false,
-            error: error,
-            message: 'Something went Wrong in Validating Evaluation',
             data: {}
         });
     }
