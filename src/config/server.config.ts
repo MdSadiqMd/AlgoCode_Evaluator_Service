0a1024ff7669d672f366e405705a885c80abe169
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-09-15 15:32:47 +0530

    Debug: port

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 4000,
     REDIS_PORT: parseInt(process.env.REDIS_PORT || "6379", 10),
-    REDIS_HOST: process.env.REDIS_HOST || '127.0.0.1',
+    REDIS_HOST: process.env.REDIS_HOST,
     BULLBOARDPATH: process.env.BULLBOARDPATH || '/admin/queues'
 };
